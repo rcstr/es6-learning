16b7a8767446125a1f444da4b4ae8974f825abb4
Rommel Castro <rcastro@prosoftnearshore.com>
2014-07-04 15:20:51 -0700

    humans!
    i added the Human|Humans module to the site #bangbang

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,6 +1,23 @@
+import Human from './models/human';
+import Humans from './models/humans';
+
 class Application {
-    constructor() {
+    constructor(human, humans) {
         $('h1').html('E6 Rocks!');
+
+        var job = new Human({id: 1, firstName:'Bob', lastName:'Morane'});
+        var rob = new Human({id: 3, firstName:'rob', lastName:'cast'});
+
+        var humans = new Humans();
+
+        humans.add(job);
+        humans.add(rob);
+
+        $('ul').html(humans.models.reduce(function(previous, current) {
+            return previous + `
+            <li>+${current.id} ${current.firstName} ${current.lastName}+</li>
+            `;
+        }, ""));
     }
 }
 
---
+++ public/js/app/models/humans.js
@@ -0,0 +1,11 @@
+class Humans {
+    constructor() {
+        this.models = [];
+    }
+
+    add(human) {
+        this.models.push(human);
+    }
+}
+
+export default Humans;
